86c14fe80b69ba23a849fe423f4946837dce57f0
Nybo <gautenybo@gmail.com>
2015-02-03 01:23:34 +0100

    La til Arraylist i Kontormeny
    For å holde kontoll over alle kontor objektene i mainlokken. Gjor også
    klassen mer dynamisk.

--- Kontormeny.java
+++ Kontormeny.java
@@ -9,10 +9,12 @@ public class Kontormeny {
 	}
 
 	public static void programlokke(){
+    ArrayList<Kontor> kontorListe = new ArrayList<Kontor>();
 
 		System.out.println("Programmet har startet!");
 		int avslutt=0;
 		int valg;
+		int antKont=0;
 		Scanner inn = new Scanner(System.in);
 		while(avslutt==0){
 
@@ -27,15 +29,22 @@ public class Kontormeny {
 	            	avslutt=1;
 	            	break;
 	            case 1:
-				    Kontor testKontoret = new Kontor('E', 4, 2,3);
-					System.out.println(testKontoret);
-					testKontoret.nyAnsatt("herman","testensen","geiboi@dsf.com","34242232",testKontoret);	            
-					testKontoret.utskriftansatte();
+	            kontorListe.add(new Kontor('E', 4, 2,3));
+
+					System.out.println(kontorListe.get(0));
+					kontorListe.get(0).nyAnsatt("herman","testensen","geiboi@dsf.com","34242232",kontorListe.get(0));	            
+					kontorListe.get(0).utskriftansatte();
+
+					System.out.println(kontorListe.get(0));
+					kontorListe.get(0).nyAnsatt("Bjorn","testensen","geiboi@dsf.com","34242232",kontorListe.get(0));	            
+					kontorListe.get(0).utskriftansatte();
 
 					break;
 	            case 2:  
 	                break;
 	            case 3:  
+
+				kontorListe.get(0).utskriftansatte();
 					break;
 	            case 4:  
 	                break;
