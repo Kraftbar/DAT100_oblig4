47e64aaddd777410cd2e447f363e616007807f86
Gaute <gautenybo@gmail.com>
2015-05-11 12:19:01 +0200

    Finder implementerer tråder

--- src/sample/Fiende.java
+++ src/sample/Fiende.java
@@ -1,7 +1,13 @@
 package sample;
 
-public class Fiende extends AbstraktRing{
+public class Fiende extends AbstraktRing implements Runnable{
     Fiende(int n,int m,int str){
         super(n,m,str);
+    }
+
+    // Her kan du bruke fks. Thread.sleep(200ms)
+    @Override
+    public void run() {
+
     }
 }
